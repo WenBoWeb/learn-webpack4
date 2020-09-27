ad253cdfa3a7deff882bd1c379237fe0e9b59a41
文文 <wenwen@B-T8NLJHD2-2021.local>
2020-09-27 15:05:06 +0800

    webpack4 css分离

---
+++ demo-1-plugin/README.md
@@ -0,0 +1,3 @@
+webpack3和webpack4中打包css分离的异同。
+
+引用其他人的博客可以看下具体写法。https://www.cnblogs.com/webtaotao/p/12108098.html
---
+++ demo-1-plugin/src/main.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+require('./main.css');
+const show = require('./home');
+// class WebpackTest extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             context: 5,
+//         }
+//     }
+//     render() {
+//         const { context } = this.state;
+//         return (
+//             <div style={{padding: '10px'}}>
+//                 hello webpack{context}
+//             </div>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<WebpackTest/>, document.querySelector('#app'));
+// console.log('hello webpack');
+show('hello webpack')
+// a?.c ? console.log(1):console.log(2)
